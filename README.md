4adfe129a7d0f5f56bdadde6444d7103adbb272a
Tremendous1192 <tremendous.duck@gmail.com>
2023-04-27 21:22:54 +0900

    将来の実装内容をコメントで記入しました

--- README.md
+++ README.md
@@ -5,10 +5,14 @@
 
 例えば、```use rand_simple::Uniform;```と宣言するだけで、一様分布乱数を使用できます。
 
-偉大な先達[rand](https://crates.io/crates/rand)と比較して、簡素なモジュール宣言による使いやすさを目指しています。
+偉大な先達[rand](https://crates.io/crates/rand)と比較して、簡素なモジュール宣言と豊富な確率変数による使いやすさを目指しています。
 
-# 乱数の周期
-使用しているアルゴリズムは[Xorshift](https://ja.wikipedia.org/wiki/Xorshift)です。
+# 実装する疑似乱数
+[計算機シミュレーションのため確率分布乱数生成法/著者　四辻 哲章/プレアデス出版](http://www.pleiades-publishing.co.jp/pdf/pdf03.html)に掲載されている確率分布乱数を実装していきます。
+
+掲載されている46種類の確率分布と、その基礎である一様分布・ベルヌーイ分布を実装していきます。
+
+基本的なアルゴリズムは[Xorshift](https://ja.wikipedia.org/wiki/Xorshift)です。
 
 Xorshiftはたった4つの状態変数から周期 $2^{128} - 1$ の乱数計算を行うことができます。
 
--- src/lib.rs
+++ src/lib.rs
@@ -38,42 +38,15 @@ pub(crate) fn update_and_uniform(_xyzw: &(Cell<u32>, Cell<u32>, Cell<u32>, Cell<
     _xyzw.3.set(w);
 
     // 乱数を返す
-    (w as f64) / (std::u32::MAX as f64)
+    (w as f64) / MAX_U32_AS_F64
 }
 
-/// ベルヌーイ分布を計算する構造体
-/// # 使用例
-/// ```
-/// use rand_simple::Bernoulli;
-/// let bernoulli = Bernoulli::new(1192u32); // コンストラクタ
-/// 
-/// // 乱数の種を返す -> 1192u32
-/// println!("乱数の種: {}", bernoulli.get_seed());
-/// 
-/// // 乱数を返す -> 0u32
-/// println!("乱数: {}", bernoulli.sample(0.5f64));
-/// ```
-pub struct Bernoulli {
-    seed: u32, // 乱数の種
-    xyzw: (Cell<u32>, Cell<u32>, Cell<u32>, Cell<u32>) // 状態変数
-}
+// 一様乱数を計算するための分母
+// 一々呼び出すよりは定数にしておいた方が計算速度が軽いのではないか?
+const MAX_U32_AS_F64: f64 = std::u32::MAX as f64;
 
-/// 幾何分布を計算する構造体
-/// # 使用例
-/// ```
-/// use rand_simple::Geometric;
-/// let geometric = Geometric::new(1192u32); // コンストラクタ
-/// 
-/// // 乱数の種を返す -> 1192u32
-/// println!("乱数の種: {}", geometric.get_seed());
-/// 
-/// // 乱数を返す -> 4u32
-/// println!("乱数: {}", geometric.sample(0.5f64));
-/// ```
-pub struct Geometric {
-    seed: u32, // 乱数の種
-    xyzw: (Cell<u32>, Cell<u32>, Cell<u32>, Cell<u32>) // 状態変数
-}
+
+// 連続型確率変数
 
 /// 一様乱数を計算する構造体
 /// # 使用例
@@ -92,4 +65,174 @@ pub struct Uniform {
     xyzw: (Cell<u32>, Cell<u32>, Cell<u32>, Cell<u32>) // 状態変数
 }
 
+// 正規分布を計算する構造体
+//struct Normal {}
 
+// 半正規分布を計算する構造体
+//struct HalfNormal {}
+
+// 対数正規分布を計算する構造体
+//struct LogNormal {}
+
+// コーシー分布を計算する構造体
+//struct Cauchy {}
+
+// レヴィ分布を計算する構造体
+//struct Levy {}
+
+// 指数分布を計算する構造体
+//struct Exponential {}
+
+// ラプラス分布を計算する構造体
+//struct Laplace {}
+
+// レイリー分布を計算する構造体
+//struct Rayleigh {}
+
+// ワイブル分布を計算する構造体
+//struct Weibull {}
+
+// ガンベル分布を計算する構造体
+//struct Gunbel {}
+
+// ガンマ分布を計算する構造体
+//struct Gamma {}
+
+// ベータ分布を計算する構造体
+//struct Beta {}
+
+// ディリクレ分布を計算する構造体
+//struct Dirichlet {}
+
+// べき関数分布を計算する構造体
+//struct PowerFunction {}
+
+// 指数べき分布を計算する構造体
+//struct ExponentialPower {}
+
+// アーラン分布を計算する構造体
+//struct Erlang {}
+
+// ガンマ二乗分布を計算する構造体
+//struct ChiSquare {}
+
+// ガンマ分布を計算する構造体
+//struct Chi {}
+
+// F分布を計算する構造体
+//struct FDistribution {}
+
+// t分布を計算する構造体
+//struct TDistribution {}
+
+// 逆ガウス分布を計算する構造体
+//struct InverseGaussian {}
+
+// 三角分布を計算する構造体
+//struct Triangular {}
+
+// パレート分布を計算する構造体
+//struct Pareto {}
+
+// ロジスティック分布を計算する構造体
+//struct Logistic {}
+
+// 双曲線正割分布を計算する構造体
+//struct HeyperbolicSecant {}
+
+// 余弦分布を計算する構造体
+//struct RaisedCosine {}
+
+// 逆正弦分布を計算する構造体
+//struct Arcsine {}
+
+// フォン・ミーゼス分布を計算する構造体
+//struct VonMises {}
+
+// 非心ガンマ分布を計算する構造体
+//struct NonCentralGamma {}
+
+// 非心ベータ分布を計算する構造体
+//struct NonCentralBeta {}
+
+// 非心ガンマ二乗分布を計算する構造体
+//struct NonCentralChiSquare {}
+
+// 非心ガンマ分布を計算する構造体
+///struct NonCentralChi {}
+
+// 非心F分布を計算する構造体
+//struct NonCentralF {}
+
+// 非心t分布を計算する構造体
+//struct NonCentralT {}
+
+// プランク分布を計算する構造体
+//struct Plank {}
+
+
+// 離散型確率変数
+
+/// ベルヌーイ分布を計算する構造体
+/// # 使用例
+/// ```
+/// use rand_simple::Bernoulli;
+/// let bernoulli = Bernoulli::new(1192u32); // コンストラクタ
+/// 
+/// // 乱数の種を返す -> 1192u32
+/// println!("乱数の種: {}", bernoulli.get_seed());
+/// 
+/// // 乱数を返す -> 0u32
+/// println!("乱数: {}", bernoulli.sample(0.5f64));
+/// ```
+pub struct Bernoulli {
+    seed: u32, // 乱数の種
+    xyzw: (Cell<u32>, Cell<u32>, Cell<u32>, Cell<u32>) // 状態変数
+}
+
+// 二項分布
+// struct Binomial {}
+
+/// 幾何分布を計算する構造体
+/// # 使用例
+/// ```
+/// use rand_simple::Geometric;
+/// let geometric = Geometric::new(1192u32); // コンストラクタ
+/// 
+/// // 乱数の種を返す -> 1192u32
+/// println!("乱数の種: {}", geometric.get_seed());
+/// 
+/// // 乱数を返す -> 4u32
+/// println!("乱数: {}", geometric.sample(0.5f64));
+/// ```
+pub struct Geometric {
+    seed: u32, // 乱数の種
+    xyzw: (Cell<u32>, Cell<u32>, Cell<u32>, Cell<u32>) // 状態変数
+}
+
+// ポアソン分布を計算する構造体
+//struct Poisson {}
+
+// 超幾何分布を計算する構造体
+//struct HeyperGeometric {}
+
+// 多項分布を計算する構造体
+//struct Multinominal {}
+
+// 負の二項分布を計算する構造体
+//struct NegativeBinomial {}
+
+// 負の超幾何分布を計算する構造体
+//struct NegativeHeyperGeometric {}
+
+// 対数級数分布を計算する構造体
+//struct LogarithmicSeries {}
+
+// ユール・シモン分布を計算する構造体
+//struct YuleSimon {}
+
+// ジップ・マンデルブロート分布を計算する構造体
+//struct ZipfMandelbrot {}
+
+// ゼータ分布を計算する構造体
+//struct Zeta {}
