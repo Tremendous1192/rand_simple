5367b404bf8597224e31a91dc4d8d5e00cbc55f2
Tremendous1192 <tremendous.duck@gmail.com>
2023-07-17 14:51:33 +0900

    0.1.10 (追加) べき関数分布

--- src/distributions.rs
+++ src/distributions.rs
@@ -1,6 +1,6 @@
 // 子モジュールに、各疑似乱数の詳細を記述する
 // 連続型確率変数
-mod beta;
+mod beta; // ベータ分布
 mod cauchy; // コーシー分布
 mod exponential; // 指数分布
 mod frechet; // フレシェ分布
@@ -16,30 +16,30 @@ mod normal; // 正規分布
 mod rayleigh; // レイリー分布
 mod reflected_weibull; // 反射ワイブル分布
 mod uniform; // 一様分布
-mod weibull; // ワイブル分布 // ベータ分布
-                             //mod dirichlet; // ディリクレ分布
-                             //mod power_function; // べき関数分布
-                             //mod exponential_power; // 指数べき分布
-                             //mod erlang; // アーラン分布
-                             //mod chi_square; // ガンマ二乗分布
-                             //mod chi; // ガンマ分布
-                             //mod f; // F分布
-                             //mod t; // t分布
-                             //mod inverse_gaussian; // 逆ガウス分布
-                             //mod triangular; // 三角分布
-                             //mod pareto; // パレート分布
-                             //mod logistic; // ロジスティック分布
-                             //mod heyperbolic_secant; // 双曲線正割分布
-                             //mod raised_cosine; // 余弦分布
-                             //mod arcsine; // 逆正弦分布
-                             //mod von_mises; // フォン・ミーゼス分布
-                             //mod non_central_gamma; // 非心ガンマ分布
-                             //mod non_central_beta; // 非心ベータ分布
-                             //mod non_central_chi_square; // 非心ガンマ二乗分布
-                             //mod non_central_chi; // 非心ガンマ分布
-                             //mod non_central_f; // 非心F分布
-                             //mod non_central_t; // 非心t分布
-                             //mod plank; // プランク分布
+mod weibull; // ワイブル分布
+             //mod dirichlet; // ディリクレ分布
+mod power_function; // べき関数分布
+                    //mod exponential_power; // 指数べき分布
+                    //mod erlang; // アーラン分布
+                    //mod chi_square; // ガンマ二乗分布
+                    //mod chi; // ガンマ分布
+                    //mod f; // F分布
+                    //mod t; // t分布
+                    //mod inverse_gaussian; // 逆ガウス分布
+                    //mod triangular; // 三角分布
+                    //mod pareto; // パレート分布
+                    //mod logistic; // ロジスティック分布
+                    //mod heyperbolic_secant; // 双曲線正割分布
+                    //mod raised_cosine; // 余弦分布
+                    //mod arcsine; // 逆正弦分布
+                    //mod von_mises; // フォン・ミーゼス分布
+                    //mod non_central_gamma; // 非心ガンマ分布
+                    //mod non_central_beta; // 非心ベータ分布
+                    //mod non_central_chi_square; // 非心ガンマ二乗分布
+                    //mod non_central_chi; // 非心ガンマ分布
+                    //mod non_central_f; // 非心F分布
+                    //mod non_central_t; // 非心t分布
+                    //mod plank; // プランク分布
 
 // 離散型確率変数
 mod bernoulli; // ベルヌーイ分布
---
+++ src/distributions/power_function.rs
@@ -0,0 +1,81 @@
+use crate::standard_distributions::xorshift160_0_1_open;
+use crate::{create_state, PowerFunction};
+
+impl PowerFunction {
+    /// コンストラクタ
+    /// * `_seed` - 乱数の種
+    pub fn new(seed: u32) -> Self {
+        let xyzuv: [u32; 5] = create_state(seed);
+        Self {
+            xyzuv,
+            shape_gamma: 1_f64,
+            min_a: 0_f64,
+            max_b: 1_f64,
+        }
+    }
+
+    /// べき関数分布に従う乱数を返す
+    pub fn sample(&mut self) -> f64 {
+        // アルゴリズム3.67: 逆関数法
+        xorshift160_0_1_open(&mut self.xyzuv).powf(-self.shape_gamma) * (self.max_b - self.min_a)
+            + self.min_a
+    }
+
+    /// 確率変数のパラメータを変更する
+    /// * `shape_gamma` - 形状母数 γ
+    /// * `min_a` - 境界母数の小範 a
+    /// * `max_b` - 境界母数の大範 b
+    pub fn try_set_params(
+        &mut self,
+        shape_gamma: f64,
+        min_a: f64,
+        max_b: f64,
+    ) -> Result<(f64, f64, f64), &str> {
+        if shape_gamma <= 0f64 {
+            Err("形状母数 γ が0以下です。確率変数のパラメータは前回の設定を維持します。")
+        } else if min_a >= max_b {
+            Err("境界母数の大範 b が小範 a と等しいあるいは大きいです。確率変数のパラメータは前回の設定を維持します。")
+        } else {
+            self.shape_gamma = shape_gamma;
+            self.min_a = min_a;
+            self.max_b = max_b;
+            Ok((shape_gamma, min_a, max_b))
+        }
+    }
+}
+
+#[macro_export]
+/// べき関数分布
+/// * `() =>` - 乱数の種は自動生成
+/// * `($seed_0: expr) =>` - 乱数の種を指定する
+/// # 使用例 1
+/// ```
+/// let mut power_function = rand_simple::create_power_function!(1192u32);
+/// println!("形状母数 γ = 1, 境界母数(小範) a = 0, 境界母数(大範) b = 1の標準べき関数分布に従う乱数を生成する -> {}", power_function.sample());
+/// ```
+/// # 使用例 2
+/// ```
+/// let mut power_function = rand_simple::create_power_function!();
+/// println!("形状母数 γ = 1, 境界母数(小範) a = 0, 境界母数(大範) b = 1の標準べき関数分布に従う乱数を生成する -> {}", power_function.sample());
+/// ```
+macro_rules! create_power_function {
+    () => {{
+        $crate::PowerFunction::new($crate::create_seed())
+    }};
+    ($seed: expr) => {
+        $crate::PowerFunction::new($seed as u32)
+    };
+}
+
+impl std::fmt::Display for PowerFunction {
+    /// println!マクロなどで表示するためのフォーマッタ
+    /// * 構造体の型
+    /// * 尺度母数
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        writeln!(f, "構造体の型: {}", std::any::type_name::<Self>())?;
+        writeln!(f, "形状母数 γ: {}", self.shape_gamma)?;
+        writeln!(f, "境界母数(小範) a: {}", self.min_a)?;
+        writeln!(f, "境界母数(大範) b: {}", self.max_b)?;
+        Ok(())
+    }
+}
