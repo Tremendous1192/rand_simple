2f21a81ec55188771685b6de85afefd1993eaa03
Tremendous1192 <tremendous.duck@gmail.com>
2024-12-30 19:40:20 +0900

    (リファクタリング)区間[0, 1)の一様乱数

--- src/standard_distributions.rs
+++ src/standard_distributions.rs
@@ -33,13 +33,13 @@ pub(crate) fn xorshift160_0_1(xyzuv: &mut [u32; 5]) -> f64 {
     return f64::from(xorshift160(xyzuv)) / MAX_U32_AS_F64;
 }
 
-// 区間[0, 1)の一様乱数
+/// 区間[0, 1)の一様乱数
 #[inline]
 pub(crate) fn xorshift160_0_1_open(xyzuv: &mut [u32; 5]) -> f64 {
     loop {
-        xorshift160(xyzuv);
+        _ = xorshift160(xyzuv);
         if xyzuv[4] != std::u32::MAX {
-            return xyzuv[4] as f64 / MAX_U32_AS_F64;
+            return f64::from(xyzuv[4]) / MAX_U32_AS_F64;
         }
     }
 }
